f1b50fc0cd84fc425df99e61d8231b350f9c0d62
Boss Codes <jadsaderkarate@gmail.com>
2024-01-06 14:52:12 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,112 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <nav className="navbar">
+    <ul className="navbar-nav">
+    <li className="logo">
+        <a href="#" class="nav-link">
+          <span class="link-text">Boss</span>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" className="svg-icon"><path d="m536-84-56-56 142-142-340-340-142 142-56-56 56-58-56-56 84-84-56-58 56-56 58 56 84-84 56 56 58-56 56 56-142 142 340 340 142-142 56 56-56 58 56 56-84 84 56 58-56 56-58-56-84 84-56-56-58 56Z"/></svg>
+        </a>
+      </li>
+
+      <li className="nav-item">
+        <a href="#" class="nav-link">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" className="svg-icon"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>
+        </svg>
+        <h4 className="link-text">Text1</h4>
+
+        </a>
+      </li>
+
+      <li className="nav-item">
+        <a href="#" class="nav-link">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" className="svg-icon"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>
+        </svg>
+        <h4 className="link-text">Text2</h4>
+
+        </a>
+      </li>
+
+      <li className="nav-item">
+        <a href="#" class="nav-link">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" className="svg-icon"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>
+        </svg>
+        <h4 className="link-text">Text3</h4>
+
+        </a>
+      </li>
+
+      <li className="nav-item">
+        <a href="#" class="nav-link">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" className="svg-icon"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>
+        </svg>
+        <h4 className="link-text">Text4</h4>
+
+        </a>
+      </li>
+
+      <li className="nav-item">
+        <a href="#" class="nav-link">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" className="svg-icon"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>
+        </svg>
+        <h4 className="link-text">Text5</h4>
+
+        </a>
+      </li>
+
+      <li className="nav-item">
+        <a href="#" class="nav-link">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" className="svg-icon"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z"/>
+        </svg>
+        <h4 className="link-text">Text6</h4>
+
+        </a>
+      </li>
+      </ul>
+    </nav>
+    <main>
+      <h1>Very Important News</h1>
+      
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id natus, corrupti incidunt dolorem ad in cupiditate dignissimos totam vitae exercitationem animi earum molestiae optio, nobis repellendus accusamus inventore, debitis quia. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas accusantium laborum dicta veniam ducimus molestiae? Doloremque accusantium deleniti eligendi! Iusto iure facilis animi porro recusandae minus facere sit quod molestias?</p>
+
+    </main>
+    </>
+  )
+}
+
+export default App
